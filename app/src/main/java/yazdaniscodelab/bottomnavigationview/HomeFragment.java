8a56d21e598250fdc94bb0297faf581f24a9b1cc
Yazdani <yaz4dani@gmail.com>
2018-05-17 14:29:15 +0600

    Third Commit

--- app/src/main/java/yazdaniscodelab/bottomnavigationview/HomeFragment.java
+++ app/src/main/java/yazdaniscodelab/bottomnavigationview/HomeFragment.java
@@ -1,11 +1,14 @@
 package yazdaniscodelab.bottomnavigationview;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 
 /**
@@ -13,17 +16,26 @@ import android.view.ViewGroup;
  */
 public class HomeFragment extends Fragment {
 
-
-    public HomeFragment() {
-        // Required empty public constructor
-    }
+    private FloatingActionButton fabBtn;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        View myview=inflater.inflate(R.layout.fragment_home, container, false);
+
+        fabBtn=myview.findViewById(R.id.fab_plus_xml);
+
+        fabBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(),"Yeah its workiong",Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(getActivity(),SecondActivity.class));
+            }
+        });
+
+        return  myview;
     }
 
 }
